db8c48a847db3398c20cb8f7e6690a02c693c7d1
Praveen Gorthy <vgorthy@paypal.com>
2014-11-18 15:48:01 -0800

    Adding log level by default in logData

--- dist/data.js
+++ dist/data.js
@@ -1,19 +1,19 @@
 "use strict";
 
 define(['angular', 'squid/index'], function (angular) {
-    angular.module('logger.data', ['squid.class'])
-        .factory('$LogData', function($Class) {
+    angular.module('logger.data', ['squid.class', 'logger.level'])
+        .factory('$LogData', function($Class,
+                                      $logLevel) {
 
-        var LogData = $Class.extend('LogData', {
-            init : function(){
-                this.payload = {};
-                this.eventName = '';
-                this.level = '';
-                this.timeStamp = new Date();
-            }
+            var LogData = $Class.extend('LogData', {
+                init : function(){
+                    this.payload = {};
+                    this.level = this.level || $logLevel.INFO;
+                    this.timeStamp = new Date();
+                }
+            });
+
+            return LogData;
         });
-
-        return LogData;
-    });
 });
 
