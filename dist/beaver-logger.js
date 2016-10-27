0dd25e5fe4de8fa89dd522c7873a386d5c8182fc
Daniel Brain <dbrain@paypal.com>
2016-10-27 09:28:31 -0700

    Dist

--- dist/beaver-logger.js
+++ dist/beaver-logger.js
@@ -194,92 +194,68 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    var meta = {};
 
-	    var _iteratorNormalCompletion = true;
-	    var _didIteratorError = false;
-	    var _iteratorError = undefined;
+	    for (var _iterator = _builders.metaBuilders, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+	        var _ref;
 
-	    try {
-	        for (var _iterator = _builders.metaBuilders[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	            var builder = _step.value;
-
-	            try {
-	                (0, _util.extend)(meta, builder(), false);
-	            } catch (err) {
-	                console.error('Error in custom meta builder:', err.stack || err.toString());
-	            }
+	        if (_isArray) {
+	            if (_i >= _iterator.length) break;
+	            _ref = _iterator[_i++];
+	        } else {
+	            _i = _iterator.next();
+	            if (_i.done) break;
+	            _ref = _i.value;
 	        }
-	    } catch (err) {
-	        _didIteratorError = true;
-	        _iteratorError = err;
-	    } finally {
+
+	        var builder = _ref;
+
 	        try {
-	            if (!_iteratorNormalCompletion && _iterator['return']) {
-	                _iterator['return']();
-	            }
-	        } finally {
-	            if (_didIteratorError) {
-	                throw _iteratorError;
-	            }
+	            (0, _util.extend)(meta, builder(), false);
+	        } catch (err) {
+	            console.error('Error in custom meta builder:', err.stack || err.toString());
 	        }
 	    }
 
-	    var _iteratorNormalCompletion2 = true;
-	    var _didIteratorError2 = false;
-	    var _iteratorError2 = undefined;
+	    for (var _iterator2 = _builders.trackingBuilders, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+	        var _ref2;
 
-	    try {
-	        for (var _iterator2 = _builders.trackingBuilders[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-	            var _builder = _step2.value;
-
-	            try {
-	                (0, _util.extend)(tracking, _builder(), false);
-	            } catch (err) {
-	                console.error('Error in custom tracking builder:', err.stack || err.toString());
-	            }
+	        if (_isArray2) {
+	            if (_i2 >= _iterator2.length) break;
+	            _ref2 = _iterator2[_i2++];
+	        } else {
+	            _i2 = _iterator2.next();
+	            if (_i2.done) break;
+	            _ref2 = _i2.value;
 	        }
-	    } catch (err) {
-	        _didIteratorError2 = true;
-	        _iteratorError2 = err;
-	    } finally {
+
+	        var _builder = _ref2;
+
 	        try {
-	            if (!_iteratorNormalCompletion2 && _iterator2['return']) {
-	                _iterator2['return']();
-	            }
-	        } finally {
-	            if (_didIteratorError2) {
-	                throw _iteratorError2;
-	            }
+	            (0, _util.extend)(tracking, _builder(), false);
+	        } catch (err) {
+	            console.error('Error in custom tracking builder:', err.stack || err.toString());
 	        }
 	    }
 
 	    var headers = {};
 
-	    var _iteratorNormalCompletion3 = true;
-	    var _didIteratorError3 = false;
-	    var _iteratorError3 = undefined;
+	    for (var _iterator3 = _builders.headerBuilders, _isArray3 = Array.isArray(_iterator3), _i3 = 0, _iterator3 = _isArray3 ? _iterator3 : _iterator3[Symbol.iterator]();;) {
+	        var _ref3;
 
-	    try {
-	        for (var _iterator3 = _builders.headerBuilders[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-	            var _builder2 = _step3.value;
-
-	            try {
-	                (0, _util.extend)(headers, _builder2(), false);
-	            } catch (err) {
-	                console.error('Error in custom header builder:', err.stack || err.toString());
-	            }
+	        if (_isArray3) {
+	            if (_i3 >= _iterator3.length) break;
+	            _ref3 = _iterator3[_i3++];
+	        } else {
+	            _i3 = _iterator3.next();
+	            if (_i3.done) break;
+	            _ref3 = _i3.value;
 	        }
-	    } catch (err) {
-	        _didIteratorError3 = true;
-	        _iteratorError3 = err;
-	    } finally {
+
+	        var _builder2 = _ref3;
+
 	        try {
-	            if (!_iteratorNormalCompletion3 && _iterator3['return']) {
-	                _iterator3['return']();
-	            }
-	        } finally {
-	            if (_didIteratorError3) {
-	                throw _iteratorError3;
-	            }
+	            (0, _util.extend)(headers, _builder2(), false);
+	        } catch (err) {
+	            console.error('Error in custom header builder:', err.stack || err.toString());
 	        }
 	    }
 
@@ -333,32 +309,24 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    payload.timestamp = Date.now();
 
-	    var _iteratorNormalCompletion4 = true;
-	    var _didIteratorError4 = false;
-	    var _iteratorError4 = undefined;
+	    for (var _iterator4 = _builders.payloadBuilders, _isArray4 = Array.isArray(_iterator4), _i4 = 0, _iterator4 = _isArray4 ? _iterator4 : _iterator4[Symbol.iterator]();;) {
+	        var _ref4;
 
-	    try {
-	        for (var _iterator4 = _builders.payloadBuilders[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-	            var builder = _step4.value;
-
-	            try {
-	                (0, _util.extend)(payload, builder(), false);
-	            } catch (err) {
-	                console.error('Error in custom payload builder:', err.stack || err.toString());
-	            }
+	        if (_isArray4) {
+	            if (_i4 >= _iterator4.length) break;
+	            _ref4 = _iterator4[_i4++];
+	        } else {
+	            _i4 = _iterator4.next();
+	            if (_i4.done) break;
+	            _ref4 = _i4.value;
 	        }
-	    } catch (err) {
-	        _didIteratorError4 = true;
-	        _iteratorError4 = err;
-	    } finally {
+
+	        var builder = _ref4;
+
 	        try {
-	            if (!_iteratorNormalCompletion4 && _iterator4['return']) {
-	                _iterator4['return']();
-	            }
-	        } finally {
-	            if (_didIteratorError4) {
-	                throw _iteratorError4;
-	            }
+	            (0, _util.extend)(payload, builder(), false);
+	        } catch (err) {
+	            console.error('Error in custom payload builder:', err.stack || err.toString());
 	        }
 	    }
 
