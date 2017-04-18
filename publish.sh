d57f30e13ce3ebe89dc9b0cf78413df4ce298737
Daniel Brain <dbrain@paypal.com>
2017-04-17 21:37:57 -0700

    Update publish script

--- publish.sh
+++ publish.sh
@@ -2,12 +2,15 @@
 
 set -e;
 
+rm -rf node_modules/sync-browser-mocks
+npm install sync-browser-mocks
+
 gulp build;
 
 git add dist;
 git commit -m "Dist" || echo "Nothing to distribute";
 
-mversion patch -m '%s';
+npm version ${1-patch};
 
 git push;
 git push --tags;
