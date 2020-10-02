8b83ff03214493a5c53ca3a8fc969c165716ac42
Daniel Brain <daniel@brainblocks.io>
2020-10-02 16:09:38 -0700

    Upgrade flow

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 /* @flow */
 /* eslint import/no-nodejs-modules: off, import/no-default-export: off */
 
+import type { WebpackConfig } from 'grumbler-scripts/config/types';
 import { getWebpackConfig } from 'grumbler-scripts/config/webpack.config';
 
 const FILE_NAME = 'beaver-logger';
 const MODULE_NAME = 'beaver';
 
-export const WEBPACK_CONFIG = getWebpackConfig({
+export const WEBPACK_CONFIG : WebpackConfig = getWebpackConfig({
     filename:   FILE_NAME,
     modulename: MODULE_NAME,
     minify:     false
 });
 
-export const WEBPACK_CONFIG_MIN = getWebpackConfig({
+export const WEBPACK_CONFIG_MIN : WebpackConfig = getWebpackConfig({
     filename:   FILE_NAME,
     modulename: MODULE_NAME,
     minify:     true
 });
 
-export const WEBPACK_CONFIG_TEST = getWebpackConfig({
+export const WEBPACK_CONFIG_TEST : WebpackConfig = getWebpackConfig({
     modulename: MODULE_NAME,
     test:       true
 });
