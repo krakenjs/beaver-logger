ac1821765059287df48165cfc40cfe927d9198b3
gregjopa <gregjopa@users.noreply.github.com>
2022-12-14 21:21:31 +0000

    ci: check in dist folder

--- dist/module/http.js
+++ dist/module/http.js
@@ -1,23 +1,19 @@
-import { ZalgoPromise } from '@krakenjs/zalgo-promise/src';
-import { request, noop } from '@krakenjs/belter/src';
-import { isSameDomain, assertSameDomain } from '@krakenjs/cross-domain-utils/src';
-import { canUseSendBeacon, isAmplitude, sendBeacon } from './util';
+import { ZalgoPromise } from "@krakenjs/zalgo-promise/src";
+import { request, noop } from "@krakenjs/belter/src";
+import { isSameDomain, assertSameDomain } from "@krakenjs/cross-domain-utils/src";
+import { canUseSendBeacon, isAmplitude, sendBeacon } from "./util";
 export function getHTTPTransport(httpWin) {
-  if (httpWin === void 0) {
-    httpWin = window;
-  }
-
-  var win = isSameDomain(httpWin) ? assertSameDomain(httpWin) : window;
   return function (_ref) {
     var url = _ref.url,
-        method = _ref.method,
-        headers = _ref.headers,
-        json = _ref.json,
-        _ref$enableSendBeacon = _ref.enableSendBeacon,
-        enableSendBeacon = _ref$enableSendBeacon === void 0 ? false : _ref$enableSendBeacon;
+      method = _ref.method,
+      headers = _ref.headers,
+      json = _ref.json,
+      _ref$enableSendBeacon = _ref.enableSendBeacon,
+      enableSendBeacon = _ref$enableSendBeacon === void 0 ? false : _ref$enableSendBeacon;
     return ZalgoPromise.try(function () {
+      var httpWindow = httpWin ? httpWin : window;
+      var win = isSameDomain(httpWindow) ? assertSameDomain(httpWindow) : window;
       var beaconResult = false;
-
       if (canUseSendBeacon({
         headers: headers,
         enableSendBeacon: enableSendBeacon
@@ -38,7 +34,6 @@ export function getHTTPTransport(httpWin) {
           });
         }
       }
-
       return beaconResult ? beaconResult : request({
         win: win,
         url: url,
--- dist/module/util.js
+++ dist/module/util.js
@@ -1,53 +1,42 @@
-import { AMPLITUDE_URL } from './config';
-
+import { AMPLITUDE_URL } from "./config";
 var canUseSendBeacon = function canUseSendBeacon(_ref) {
   var headers = _ref.headers,
-      enableSendBeacon = _ref.enableSendBeacon;
+    enableSendBeacon = _ref.enableSendBeacon;
   var hasHeaders = headers && Object.keys(headers).length;
-
   if (window && window.navigator.sendBeacon && !hasHeaders && enableSendBeacon && window.Blob) {
     return true;
   }
-
   return false;
 };
-
 var isAmplitude = function isAmplitude(url) {
   if (url === AMPLITUDE_URL) {
     return true;
   }
-
   return false;
 };
-
 var sendBeacon = function sendBeacon(_ref2) {
   var _ref2$win = _ref2.win,
-      win = _ref2$win === void 0 ? window : _ref2$win,
-      url = _ref2.url,
-      data = _ref2.data,
-      _ref2$useBlob = _ref2.useBlob,
-      useBlob = _ref2$useBlob === void 0 ? true : _ref2$useBlob;
-
+    win = _ref2$win === void 0 ? window : _ref2$win,
+    url = _ref2.url,
+    data = _ref2.data,
+    _ref2$useBlob = _ref2.useBlob,
+    useBlob = _ref2$useBlob === void 0 ? true : _ref2$useBlob;
   try {
     var json = JSON.stringify(data);
-
     if (!win.navigator.sendBeacon) {
       throw new Error("No sendBeacon available");
     }
-
     if (useBlob) {
       var blob = new Blob([json], {
-        type: 'application/json'
+        type: "application/json"
       });
       return win.navigator.sendBeacon(url, blob);
     }
-
     return win.navigator.sendBeacon(url, json);
   } catch (e) {
     return false;
   }
 };
-
 var extendIfDefined = function extendIfDefined(target, source) {
   for (var key in source) {
     if (source.hasOwnProperty(key)) {
@@ -55,5 +44,4 @@ var extendIfDefined = function extendIfDefined(target, source) {
     }
   }
 };
-
 export { canUseSendBeacon, extendIfDefined, isAmplitude, sendBeacon };
